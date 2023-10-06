87636f591225b7c2b3890d98768a858e940d9da5
Kushagrashrivastava <kushagra10june@gmail.com>
2023-10-06 12:24:11 +0530

    updateing commit

--- src/components/StudentPortal.js
+++ src/components/StudentPortal.js
@@ -38,7 +38,7 @@ const StudentPortal = () => {
 
     <h2>Document</h2>
     <div style={rectangle}>
-        Document
+        Document we will be uploading this portol soon
         </div>
     <h2>Fetch Documents</h2>
     <div style={rectangle}>Fetch Documents</div>
